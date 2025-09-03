dcf95a38a5e31addbd55591856980a5f95ad44c6
Teejayskillz <Teejayadewale87@gmail.com>
2025-09-04 00:55:26 +0100

    added post image link , cat and page description

--- core/models.py
+++ core/models.py
@@ -45,6 +45,7 @@ class SiteSettings(models.Model):
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(max_length=100, unique=True)
+    description = models.TextField(blank=True)
 
     def __str__(self):
         return self.name
