02a7487e12f0954379d4c0897f2d074bf8081567
Teejayskillz <teejayadewale87@gmail.com>
2025-12-28 18:40:51 +0100

    added feeds management commands

---
+++ core/management/commands/fetchfeeds.py
@@ -0,0 +1,10 @@
+from django.core.management.base import BaseCommand
+from feeds.utils import fetch_feeds
+
+
+class Command(BaseCommand):
+    help = "Fetch external posts into blog"
+
+    def handle(self, *args, **kwargs):
+        fetch_feeds()
+        self.stdout.write(self.style.SUCCESS("Feeds imported"))
